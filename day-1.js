143fcea0a7557bae842b685bab648fa8b18f0a2a
Anuj kumar <anujkumarmharaja@gmail.com>
2024-07-27 13:34:34 +0530

    day1 of 30days javascript challenge

---
+++ day-1.js
@@ -0,0 +1,49 @@
+// activity 1 variable declearation
+
+// task 1
+var num =  2024;
+console.log(num)
+
+// task 2
+let course = "javascript";
+console/log(course)
+
+// activity 2 constant declearation
+
+// task 3
+const isBool = true; 
+console.log(isBool);
+
+// activity 3 Data types 
+
+// task 4
+let num = 5 ;
+let str = "30daysJavascripChallenge";
+let bool = true;
+let userInfo = {
+  username : "anuj",
+  password : "javaScript"
+}
+let arr = [1 ,2 , 3 ,4 , 5]
+console.log(typeof(num))
+console.log(typeof(str))
+console.log(typeof(bool))
+console.log(typeof(userInfo))
+console.log(typeof(arr))
+
+// activity 4 reassigning Varibles 
+
+// task 5
+let a = 2020 ;
+console.log("intial value of a is " + a)
+a = 2024 
+console.log("New value of a is "+ a)
+
+// activity 5 understanding const  
+
+// task 6
+const name = "Susan";
+console.log(name)
+name = "anuj"
+console.log(name)
+//TypenError : Assignment to constant variable.
