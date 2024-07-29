26fa5fc8d028e3e98ccc92084525f765df7b3b80
Anuj kumar <anujkumarmharaja@gmail.com>
2024-07-29 22:12:24 +0530

    add activity 3 and 4 in day7

--- day-7.js
+++ day-7.js
@@ -40,3 +40,43 @@ const book = {
   }
   book2.updateYear("2020")
   console.log("the update year is",book2.year)
+
+
+  // activity 3 object methods
+
+// task 5
+const library =
+{
+  name : "collage library",
+  books :[
+    { title : "book1",author : "xyz",year : "2023"
+      } , {
+      title : "book2",author : "abc",year : "2024"
+      }
+  ]
+}
+
+console.log(library)
+
+
+// task 6 
+// name of the library
+console.log("the name of the library is",library.name)
+// book's title in the library
+for(let i =0 ; i <library.books.length ; i++)
+{
+  console.log("the title of the book is",library.books[i].title)
+}
+
+// activity 4 The this keyword
+
+// task 7
+let bookObject = {
+name : "theory of javascript",
+author : "Ram",
+year : 2022,
+getTitleAndYear : function(){
+  return ("the book "+this.name+" was written by "+this.author+" in the year "+this.year)
+}
+}
+console.log(bookObject.getTitleAndYear())
